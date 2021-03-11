acbe9a74f8aa1dc81ace8797df244e58eb9e5c1a
RichJames <risajajr@gmail.com>
2021-03-11 13:01:27 -0600

    Final part1, day 21 solution.

--- 2015/Day21.lisp
+++ 2015/Day21.lisp
@@ -7,45 +7,6 @@
 
 (in-package #:Day21)
 
-(defclass player ()
-  ((hit-points  :initarg :hit-points :accessor hit-points)
-   (damage      :initarg :damage     :accessor damage)
-   (armor       :initarg :armor      :accessor armor)
-   (spend       :initarg :spend      :accessor spend)))
-
-(defparameter *boss* (make-instance 'player
-                                    :hit-points 104 
-                                    :damage       8
-                                    :armor        1
-                                    :spend        0))
-
-(defparameter *me* (make-instance 'player
-                                  :hit-points 100 
-                                  :damage       0
-                                  :armor        0
-                                  :spend        0))
-
-;; Items are: '(name cost damage armor)
-
-(defparameter *weapons* '((dagger      8 4 0)
-                          (shortsword 10 5 0)
-                          (warhammer  25 6 0)
-                          (longsword  40 7 0)
-                          (greataxe   74 8 0)))
-
-(defparameter *armor* '((leather     13 0 1)
-                        (chainmail   31 0 2)
-                        (splintmail  53 0 3)
-                        (bandedmail  75 0 4)
-                        (platemail  102 0 5)))
-
-(defparameter *rings* '((damage+1   25 1 0)
-                        (damage+2   50 2 0)
-                        (damage+3  100 3 0)
-                        (defense+1  20 0 1)
-                        (defense+2  40 0 2)
-                        (defense+3  80 0 3)))
-
 ;; I must buy:
 ;;  - exactly 1 weapon
 ;;  - 0 - 1 armor
@@ -57,47 +18,56 @@
 ;;
 ;;  What is the least amount of gold I can spend and still win the fight?
 
-;; With no armor, after 13 rounds I will lose.  The boss does 8 damage and I have 100 hit points.
-;; I need to inflict at least 8 damage each round to win.  That means I need weapons that inflict at
-;; least 9 damage (the boss has 1 armor).
-
 ;; I need to find the cheapest solution where rounds to kill me >= rounds to kill the boss.
-;; rounds to kill me   = (/ my-hit-points (- boss-damage my-armor))
-;; rounds to kill boss = (/ boss-hit-points (- my-damage boss-armor))
-;;  I.E.
-;; rounds to kill me   = (/ 100 (- 8 my-armor))
-;; rounds to kill boss = (/ 104 (- my-damage 1))
-;;
-;; So, (/ 100 (- 8 my-armor) >= (/ 104 (- my-damage 1))
-;; Reduced, this is 25*my-damage + 26*my-armor >= 233
-;;
-;; Expressing my-damage and my-armor in terms of each other, we get:
-;; my-armor  >= (/ (- 233 (* 25 my-damage)) 26)
-;; my-damage >= (/ (- 233 (* 26 my-armor)) 25)
+;;   rounds to kill me   = (ceiling (/ my-hit-points (- boss-damage my-armor)))
+;;   rounds to kill boss = (ceiling (/ boss-hit-points (- my-damage boss-armor)))
+;; Note that I have to compute the ceiling of these values.  Rounds are discrete units of time.
 
 ;; I have to make a list of all the possible equipment combinations I am allowed to buy, then assess which of
 ;; these would guarantee a win.  Of those, I need to pick the cheapest option.
 
-(defparameter *weapons-list* '(dagger shortsword warhammer longsword greataxe))
-(defparameter *armor-list* '(leather chainmail splintmail bandedmail platemail))
-(defparameter *rings-list* '(damage+1 damage+2 damage+3 defense+1 defense+2 defense+3))
+;; Attributes are: '(hit-points damage armor)
+(defparameter *boss* '(104 8 12))
+(defparameter *me*   '(100 0  0))
 
-(defparameter *combinations* nil)
+;; Items are: '(name cost damage armor)
+(defparameter *weapons* '((dagger      8 4 0)
+                          (shortsword 10 5 0)
+                          (warhammer  25 6 0)
+                          (longsword  40 7 0)
+                          (greataxe   74 8 0)))
+
+(defparameter *armor* '((leather     13 0 1)
+                        (chainmail   31 0 2)
+                        (splintmail  53 0 3)
+                        (bandedmail  75 0 4)
+                        (platemail  102 0 5)
+                        (none         0 0 0)))
+
+(defparameter *rings* '((damage+1   25 1 0)
+                        (damage+2   50 2 0)
+                        (damage+3  100 3 0)
+                        (defense+1  20 0 1)
+                        (defense+2  40 0 2)
+                        (defense+3  80 0 3)
+                        (none-1      0 0 0)
+                        (none-2      0 0 0)))
+
+(defun find-part1-answer ()
+  (find-cheapest-winner (find-winning-combos (build-part1-combos))))
 
 (defun build-part1-combos ()
-  (build-item-combos *weapons-list* *armor-list* *rings-list*))
+  (build-item-combos *weapons* *armor* *rings*))
 
 (defun build-item-combos (weapons-list armor-list rings-list)
-  (append (build-only-weapons-combos weapons-list)
-          (build-weapon-and-armor-combos weapons-list armor-list)
-          (build-weapon-and-ring-combos weapons-list rings-list)
-          (build-weapon-armor-and-ring-combos weapons-list armor-list rings-list)))
-
-(defun validate-item-combos (combos)
-  (loop :for combo :in combos
-        :if (not (valid-item-combo-p combo))
-          :return combo
-        :finally (return t)))
+  (loop :with combos = nil
+        :for weapon :in weapons-list
+        :do (loop :for armor :in armor-list
+                  :do (loop :for ring1 :in rings-list
+                            :for ring2-list = (remove ring1 rings-list :test #'equal)
+                            :do (loop :for ring2 :in ring2-list
+                                      :do (setf combos (append combos (list (list (car weapon) (car armor) (car ring1) (car ring2))))))))
+        :finally (return combos)))
 
 (defun find-winning-combos (combos)
   (loop :for combo :in combos
@@ -116,91 +86,37 @@
                   (setf cheapest-cost combo-cost)))
         :finally (return (values cheapest-cost best-combo))))
 
-(defun compute-winner-costs (combos)
-  (loop :for combo :in combos
-        :collect (list (combo-cost combo) combo) :into combo-costs
-        :finally (return (sort combo-costs #'< :key 'car))))
-
 (defun combo-cost (combo)
-  (let* ((weapon            (intersection combo *weapons-list*))
-         (weapon-cost       (nth 1 (find (car weapon) *weapons* :key 'car)))
-         (armor             (intersection combo *armor-list*))
-         (armor-cost        (if armor (nth 1 (find (car armor) *armor* :key 'car)) 0))
-         (rings             (intersection combo *rings-list*)))
-    (loop :for ring :in rings
-          :sum (nth 1 (find ring *rings* :key 'car)) :into rings-cost
-          :finally (return (+ weapon-cost armor-cost rings-cost)))))
+  ;; combo will be a list: '(weapon armor ring1 ring2)
+  (let ((weapon-cost   (nth 1 (find (nth 0 combo) *weapons* :key 'car)))
+        (armor-cost    (nth 1 (find (nth 1 combo) *armor*   :key 'car)))
+        (ring1-cost    (nth 1 (find (nth 2 combo) *rings*   :key 'car)))
+        (ring2-cost    (nth 1 (find (nth 3 combo) *rings*   :key 'car))))
+    (+ weapon-cost armor-cost ring1-cost ring2-cost)))
 
 (defun winning-combo-p (combo)
   (let* ((my-attack           (my-damage combo))         
          (my-armor            (my-defense combo))
-         (my-hit-points       (hit-points *me*))
-         (my-net-damage       (max 1 (- my-attack (armor *boss*))))
-         (boss-net-damage     (max 1 (- (damage *boss*) my-armor)))
-         (rounds-to-kill-me   (/ my-hit-points boss-net-damage))
-         (rounds-to-kill-boss (/ (hit-points *boss*) my-net-damage)))
+         (my-net-damage       (max 1 (- my-attack (nth 2 *boss*))))
+         (boss-net-damage     (max 1 (- (nth 1 *boss*) my-armor)))
+         (rounds-to-kill-me   (ceiling (/ (nth 0 *me*)   boss-net-damage)))
+         (rounds-to-kill-boss (ceiling (/ (nth 0 *boss*) my-net-damage))))
     (<= rounds-to-kill-boss rounds-to-kill-me)))
 
 (defun my-defense (combo)
-  (let* ((armor         (intersection combo *armor-list*))
-         (rings         (intersection combo *rings-list*))
-         (armor-defense (if armor (nth 3 (find (car armor) *armor* :key 'car)) 0)))
-    (loop :for ring :in rings
-          :sum (nth 3 (find ring *rings* :key 'car)) :into ring-defense
-          :finally (return (+ armor-defense ring-defense)))))
+  ;; combo will be a list: '(weapon armor ring1 ring2)
+  (let ((armor-defense  (nth 3 (find (nth 1 combo) *armor* :key 'car)))
+        (ring1-defense  (nth 3 (find (nth 2 combo) *rings* :key 'car)))
+        (ring2-defense  (nth 3 (find (nth 3 combo) *rings* :key 'car))))
+    (+ armor-defense ring1-defense ring2-defense)))
 
 (defun my-damage (combo)
-  (let* ((weapon        (intersection combo *weapons-list*))
-         (weapon-damage (nth 2 (find (car weapon) *weapons* :key 'car)))
-         (rings         (intersection combo *rings-list*)))
-    (loop :for ring :in rings
-          :sum (nth 2 (find ring *rings* :key 'car)) :into ring-damage
-          :finally (return (+ weapon-damage ring-damage)))))
+  ;; combo will be a list: '(weapon armor ring1 ring2)
+  (let ((weapon-damage  (nth 2 (find (nth 0 combo) *weapons* :key 'car)))
+        (ring1-damage   (nth 2 (find (nth 2 combo) *rings*   :key 'car)))
+        (ring2-damage   (nth 2 (find (nth 3 combo) *rings*   :key 'car))))
+    (+ weapon-damage ring1-damage ring2-damage)))
 
-(defun build-only-weapons-combos (weapons-list)
-  (loop :for weapon :in weapons-list
-        :collect (list weapon)))
-
-(defun build-weapon-and-armor-combos (weapons-list armor-list)
-  (loop :with combos = nil
-        :for weapon :in weapons-list
-        :do (setf combos (append combos (loop :for armor :in armor-list
-                                              :collect (list weapon armor))))
-        :finally (return combos)))
-
-(defun build-weapon-and-ring-combos (weapons-list rings-list)
-  (loop :with combos = nil
-        :with ring-combos = (build-ring-combos rings-list)
-        :for weapon :in weapons-list
-        :do (setf combos (append combos (loop :for ring-list :in ring-combos
-                                              :collect (append (list weapon) ring-list))))
-        :finally (return combos)))
-
-(defun build-weapon-armor-and-ring-combos (weapon-list armor-list rings-list)
-  (loop :with combos = nil
-        :with weapon-ring-combos = (build-weapon-and-ring-combos weapon-list rings-list)
-        :for armor :in armor-list
-        :do (setf combos (append combos (loop :for weapon-ring-combo in weapon-ring-combos
-                                              :collect (append (list armor) weapon-ring-combo))))
-        :finally (return combos)))
-
-(defun build-ring-combos (rings-list)
-  (append (build-single-ring-combos rings-list)
-          (build-two-ring-combos rings-list)))
-
-(defun build-single-ring-combos (rings-list)
-  (loop :for ring :in rings-list
-        :collect (list ring)))
-
-(defun build-two-ring-combos (rings-list)
-  (loop :with combos = nil
-        :with ring2-list = rings-list
-        :for ring1 :in rings-list
-        :do (progn
-              (setf ring2-list (remove ring1 ring2-list))
-              (setf combos (append combos (loop :for ring2 :in ring2-list
-                                               :collect (list ring1 ring2)))))
-        :finally (return combos)))
 
 
 ;; This is how to turn two lists into a list of combinations, e.g. given
