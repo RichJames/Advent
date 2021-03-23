550785b0a72675e083fb639153663b0fd611f81c
RichJames <risajajr@gmail.com>
2021-03-23 11:13:48 -0500

    Refactored code to tighten it up.

--- 2015/Day25.lisp
+++ 2015/Day25.lisp
@@ -17,43 +17,46 @@
     (rem (* code multiplier) divisor)))
 
 (defun prior-cell (row col)
-  (cond ((= col row 1) (list row col))
-        ((= col 1) (list 1 (- row 1)))
-        (t (list (+ row 1) (- col 1)))))
+  (cond ((= col row 1) (values row col))
+        ((= col 1)     (values 1 (- row 1)))
+        (t             (values (+ row 1) (- col 1)))))
 
 (defun next-cell (row col)
-  (cond ((= row col 1) (list 2 1))
-        ((= row 1) (list (+ row col) 1))
-        (t (list (- row 1) (+ col 1)))))
+  (cond ((= row col 1) (values 2 1))
+        ((= row 1)     (values (+ row col) 1))
+        (t             (values (- row 1) (+ col 1)))))
 
 (defun find-matrix-size (row col)
-  (loop :with max-row = row :and max-col = col
-        :for (r c) = (prior-cell row col) :then (prior-cell r c)
-        :do (progn
-              (if (> r max-row) (setf max-row r))
-              (if (> c max-col) (setf max-col c)))
-        :until (= c 1)
-        :finally (let ((max-dim (max max-row max-col)))
-                   (return (list max-dim max-dim)))))
+  (cond ((= row col 1)             (values row col))
+        ((= col 1)                 (values row (nth-value 1 (prior-cell row col))))
+        ((= row 1)                 (values col col))
+        (t (loop :for (r c) = (multiple-value-list (prior-cell row col))
+                   :then (multiple-value-list (prior-cell r c))
+                 :when (= c 1)
+                   :return (values r (1- r))))))
 
 (defun get-code (row col)
-  (let* ((matrix-dims  (find-matrix-size row col))
-         (matrix-rows  (car matrix-dims))
-         (matrix-cols  (cadr matrix-dims))
-         (codes    (make-array matrix-dims)))
+  (multiple-value-bind (matrix-rows matrix-cols) (find-matrix-size row col)
+    (let ((codes  (make-array (list matrix-rows matrix-cols))))
 
-    (setf (aref codes 0 0) *first-code*)
+      (setf (aref codes 0 0) *first-code*)
 
-    (loop :for (r c) = (next-cell 1 1) :then (next-cell r c)
-          :until (or (> r matrix-rows) (> c matrix-cols))          
-          :do (let* ((prev-cell (prior-cell r c))
-                     (prev-row  (car prev-cell))
-                     (prev-col  (cadr prev-cell))
-                     (prev-code (aref codes (1- prev-row) (1- prev-col))))
-                (setf (aref codes (1- r) (1- c)) (next-code prev-code))))
+      (loop :for (r c) = (multiple-value-list (next-cell 1 1))
+              :then (multiple-value-list (next-cell r c))
+            :until (or (> r matrix-rows) (> c matrix-cols))          
+            :do (multiple-value-bind (prev-row prev-col) (prior-cell r c)
+                  (let ((prev-code (aref codes (1- prev-row) (1- prev-col))))
+                    (setf (aref codes (1- r) (1- c)) (next-code prev-code)))))
 
-    (aref codes (1- row) (1- col))))
+      (aref codes (1- row) (1- col)))))
 
 (defun part1 ()
   (get-code 2978 3083))
+
+
+
+
+
+
+
 
