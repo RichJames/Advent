cebed28d53402d20805c2ac943555b8c38aae6fe
RichJames <risajajr@gmail.com>
2021-04-09 16:20:38 -0500

    Solution to part2, day 4.

--- 2016/Day04.lisp
+++ 2016/Day04.lisp
@@ -47,3 +47,37 @@
           :if (valid-checksum-p parsed-r)
             :collect (advent-room-id parsed-r) :into valid-room-ids
           :finally (return (reduce #'+ valid-room-ids)))))
+
+;;; ***** Part2 *****
+
+(defparameter *room-to-find* "North Pole")
+
+(defun shift-letter (c shift)
+  (let* ((shift-amount (rem shift 26))
+         (char-code-a  (char-code #\a))
+         (c-code       (- (char-code c) char-code-a)))
+    (if (equal c #\-)
+        #\Space
+        (code-char (+ (rem (+ c-code shift-amount) 26) char-code-a)))))
+
+(defun decrypt-room-name (room-name shift)
+  (loop :for c :across room-name
+        :collect (shift-letter c shift) :into decryption-list
+        :finally (return (coerce decryption-list 'string))))
+
+(defun get-encrypted-name-and-id (room)
+  (let* ((last-dash     (position #\- room :from-end t))
+         (room-name     (subseq room 0 last-dash))
+         (first-bracket (position #\[ room))
+         (room-id       (parse-integer (subseq room (1+ last-dash) first-bracket))))
+    (values room-name room-id)))
+
+(defun part2 ()
+  (with-open-file (stream "~/quicklisp/local-projects/rich/advent/2016/Day04.txt")
+    (loop :for r = (read-line stream nil nil)
+          :while r
+          :for parsed-r = (parse-encrypted-room-name r) :then (parse-encrypted-room-name r)
+          :if (valid-checksum-p parsed-r)
+            :collect (multiple-value-bind (name id) (get-encrypted-name-and-id r)
+                       (list id (decrypt-room-name name id))) :into decrypted-rooms
+          :finally (return (find-if #'(lambda (x) (search "north" (cadr x))) decrypted-rooms)))))
