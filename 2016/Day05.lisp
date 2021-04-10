de50a1358238556bd5a94fcbccfd32006c11bd78
RichJames <risajajr@gmail.com>
2021-04-10 18:27:12 -0500

    Solution to part2, Day 5.

--- 2016/Day05.lisp
+++ 2016/Day05.lisp
@@ -34,9 +34,36 @@
         :for i :from 0
         :until (= count 8)
         :for hash = (md5hash i) :then (md5hash i)
-        :when (5-zeros-p (md5hash i))
+        :when (5-zeros-p hash)
           :collect (progn
                      (incf count)
                      (format t "Found: ~a~%" hash)
                      hash) :into hashes
         :finally (return (mapcar #'(lambda (x) (subseq x 5 6)) hashes))))
+
+;;; ***** Part 2 *****
+
+(defparameter *code* (make-array 8 :initial-element nil))
+
+(defun reset2 ()
+  (reset)
+  (setf *code* (make-array 8 :initial-element nil)))
+
+(defun update-code (hash)
+  (let ((index   (parse-integer (subseq hash 5 6) :junk-allowed t))
+        (char-7  (elt hash 6)))
+    (if (and index (>= index 0) (< index 8) (not (aref *code* index)))
+        (progn
+          (format t "Index: ~a, code: ~a~%" index char-7)
+          (setf (aref *code* index) char-7)))))
+
+(defun part2 ()
+  (reset2)
+  (loop :with count = 0
+        :for i :from 0
+        :until (= count 8)
+        :for hash = (md5hash i) :then (md5hash i)
+        :when (5-zeros-p hash)
+          :if (update-code hash)
+            :do (incf count)
+        :finally (return (coerce *code* 'string))))
