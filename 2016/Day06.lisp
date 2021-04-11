2e3efbb68bc6c62087dd7431cb874aa7dff8bd5f
RichJames <risajajr@gmail.com>
2021-04-11 12:39:04 -0500

    Solution to part1, day 06.

---
+++ 2016/Day06.lisp
@@ -0,0 +1,47 @@
+;;;;
+;;;;  Advent of Code 2016
+;;;;     Day 06
+
+(defpackage #:2016Day06
+  (:use #:cl))
+
+(in-package #:2016Day06)
+
+(defparameter *letter-counts* (make-array '(8 26) :initial-element 0))
+
+(defparameter *input* "~/quicklisp/local-projects/rich/advent/2016/Day06.txt")
+
+(defun reset ()
+  (setf *letter-counts* (make-array '(8 26) :initial-element 0)))
+
+(defun count-letters (line)
+  (loop :with code-for-a = (char-code #\a)
+        :for i :upto (length line)
+        :for l :across line
+        :for l-code = (- (char-code l) code-for-a)
+        :do (incf (aref *letter-counts* i l-code))))
+
+(defun count-input (file)
+  (reset)
+  (with-open-file (stream file)
+    (loop :for line = (read-line stream nil nil)
+          :while line
+          :do (count-letters line))))
+
+(defun decode-input ()
+  (loop :with message = nil
+        :for i :below 8
+        :do (loop :with code-for-a = (char-code #\a)
+                  :for j :below 26
+                  :for count = (aref *letter-counts* i j)
+                  :for c-code = code-for-a :then (1+ c-code)
+                  :for letter = (code-char c-code)
+                  :collect (list letter count) :into results
+                  :finally (push (list (caar (stable-sort results #'> :key 'cadr))) message))
+        :finally (return (reverse message))))
+
+(defun part1 ()
+  (reset)
+  (count-input *input*)
+  (decode-input))
+
