8dd3274e893f4f3457f252f9e2e6f5116f0f90b4
RichJames <risajajr@gmail.com>
2021-05-08 19:30:14 -0500

    Added queue & start of breadth-first search.

--- 2016/Day11.lisp
+++ 2016/Day11.lisp
@@ -29,12 +29,6 @@
 (defparameter *end-goal* #b1100000000000000000000000000000000000000001111111111)
 
 
-
-
-;;; Only need this if I am working with bit vectors
-(defun bitvec->integer (bitvec)
-  (reduce (lambda (a b) (+ (* 2 a) b)) bitvec))
-
 (defmacro e-floor ()
   `(ldb (byte 2 50) *facility*))
 
@@ -107,10 +101,6 @@
                   (setf safe nil)))
         :finally (return safe)))
 
-(defun count-bits (bits &optional (size 5))
-  (loop :for i :below size
-        :sum (ldb (byte 1 i) bits)))
-
 (defun pick-up (&key (chips 0) (generators 0))
   (let* ((floor-chips   (chip-bits (floor-bits (e-floor))))
          (floor-gens    (gen-bits (floor-bits (e-floor))))
@@ -118,17 +108,21 @@
          (new-e-gens    (logior (gen-bits (e-bits)) generators))
          (new-fl-chips  (logxor floor-chips chips))
          (new-fl-gens   (logxor floor-gens generators)))
-    
-    (if (<= (+ (count-bits new-e-chips) (count-bits new-e-gens)) 2)
 
-        (progn
-          (if (and (/= chips 0) (= chips (logand chips floor-chips)))
-              (setf (chip-bits (e-bits)) new-e-chips
-                    (chip-bits (floor-bits (e-floor))) new-fl-chips))
+    (flet ((count-bits (bits)
+             (loop :for i :below 5
+                   :sum (ldb (byte 1 i) bits))))
+      
+      (if (<= (+ (count-bits new-e-chips) (count-bits new-e-gens)) 2)
 
-          (if (and (/= generators 0) (= generators (logand generators floor-gens)))
-              (setf (gen-bits (e-bits)) new-e-gens
-                    (gen-bits (floor-bits (e-floor))) new-fl-gens))))))
+          (progn
+            (if (and (/= chips 0) (= chips (logand chips floor-chips)))
+                (setf (chip-bits (e-bits)) new-e-chips
+                      (chip-bits (floor-bits (e-floor))) new-fl-chips))
+
+            (if (and (/= generators 0) (= generators (logand generators floor-gens)))
+                (setf (gen-bits (e-bits)) new-e-gens
+                      (gen-bits (floor-bits (e-floor))) new-fl-gens)))))))
 
 (defun drop-off (&key (chips 0) (generators 0))
   (let* ((e-chips      (chip-bits (e-bits)))
@@ -149,13 +143,80 @@
 (defun print-bits (n size)
   (format t "~v,'0b" size (ldb (byte size 0) n)))
 
-(defparameter *prev-states* (make-hash-table))
+
+
+
+
+;;; ***** Breadth-first search *****
+
+;;; To do a breadth-first search, I need to store two values with each "node" or, in this case state, that I examine.
+;;; These are: the distance from the original state, which can be computed by adding 1 to the distance its neighbor with
+;;; the shortest distance has, and it needs to store the ID of that neighbor.
+;;;
+;;; For this problem, each state of the factory is a node. The state itself (aka the value of *factory* at that point in
+;;; time) is the id of that state.  I can store these in a hash table.  The value I need to store with the corresponding
+;;; hash is a struct containing the two values mentioned above.
+;;;
+;;; To find possible neighbors, I need to produce a set of possible moves I can do from a given state.  Then, each of those
+;;; states needs to be tested to see if it results in either the end state we are seeking or a new, safe state that we haven't
+;;; seen before.  Any move that results in a previously seen state or in an unsafe state needs to be rejected.
+;;;
+;;; If we find the end state, we are done: we have found how to get to the end state in the fewest moves.
+;;;
+;;; If we find a new, safe state, we need to enqueue it and also record it in the hash table, using the prior state as
+;;; the previous node value and adding 1 to that node's distance to be the new node's distance.
+;;;
+;;; The process of finding new neighbors (mentioned above) is driven by emptying the queue (using a FIFO approach).  So
+;;; for each enqueued node, I want to do the process of finding the valid neighbors, recording them and enqueueing them.
+;;;
+;;; Therefore, to start the process, I should record the initial state in the hash table with a distance of 0 and its
+;;; previous node values set to nil.  I should also enqueue this node.  Then, I need to kick-off a process (loop) that
+;;; processes each entry in the queue (FIFO) to find neighbors and record and enqueue them until the end state is found.
+
+;;; **** queue implementations from Common Lisp Recipes ****
+(defclass queue ()
+  ((list :initform nil)
+   (tail :initform nil)))
+
+(defmethod dequeue ((queue queue))
+  (with-slots (list) queue
+    (pop list)))
+
+(defmethod enqueue (new-item (queue queue))
+  (with-slots (list tail) queue
+    (let ((new-tail (list new-item)))
+      (cond ((null list) (setf list new-tail))
+            (t (setf (cdr tail) new-tail)))
+      (setf tail new-tail))))
+
+(defmethod print-object ((queue queue) stream)
+  (print-unreadable-object (queue stream :type t)
+    (with-slots (list tail) queue
+      (cond ((cddddr list)
+             ;; at least five elements, so print ellipsis
+             (format stream "(~{~S ~}... ~S)" (subseq list 0 3) (first tail)))
+            ;; otherwise, print whole list
+            (t (format stream "~:S" list))))))
+
+(defparameter *queue* (make-instance 'queue))
+
+;;; **** end of queue implementation ****
+
+(defclass node-info ()
+  ((distance :initform 0)
+   (prev-node :initform nil)))
+
+(defparameter *nodes* (make-hash-table))
 
 (defun reset ()
   (setf *facility* #b0000000000001000010000000000111101111000000000000000)
-  (clrhash *prev-states*)
-  (setf (gethash *facility* *prev-states*) t))
+  (clrhash *nodes*)
+  (setf (gethash *facility* *nodes*) (make-instance 'node-info))
+  (setf *queue* (make-instance 'queue))
+  (enqueue *facility* *queue*))
 
+;; This move function is mostly correct, but I need to review how I am using the global *facility* in this code.  I need to
+;; ensure that *facility* really does contain the true previous state for this given call.
 (defun move (direction &key (chips 0) (generators 0))
   (let ((prev-state *facility*))
     (pick-up :chips chips :generators generators)
@@ -163,8 +224,13 @@
     (drop-off :chips chips :generators generators)
 
     (if (or (not (is-safe-p))
-            (gethash *facility* *prev-states*))
+            (gethash *facility* *nodes*))
         (setf *facility* prev-state))
 
-    (if (not (gethash *facility* *prev-states*))
-        (setf (gethash *facility* *prev-states*) t))))
+    (if (not (gethash *facility* *nodes*))
+        (let* ((prev-info (gethash prev-state *nodes*))
+               (prev-dist (slot-value prev-info distance)))
+          (enqueue *facility* *queue*)
+          (setf (gethash *facility* *nodes*) (make-instance 'node-info
+                                                            :distance (1+ prev-dist)
+                                                            :prev-node prev-state))))))
