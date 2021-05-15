a00560c84e7ace55fb8614028d22914b66e438b0
RichJames <risajajr@gmail.com>
2021-05-15 09:42:58 -0500

    Solution to part 1, Day 15.

---
+++ 2016/Day15.lisp
@@ -0,0 +1,35 @@
+;;;;
+;;;;  Advent of Code 2016
+;;;;     Day 15
+
+(defpackage #:2016Day15
+  (:use #:cl))
+
+(in-package #:2016Day15)
+
+(defparameter *discs* #((13 1) (19 10) (3 2) (7 1) (5 3) (17 5)))
+
+(defun test-advance-disc (disc-idx steps)
+  (let ((slots     (car (elt *discs* disc-idx)))
+        (curr-slot (cadr (elt *discs* disc-idx))))
+    (mod (+ curr-slot steps) slots)))
+
+(defun advance-discs ()
+  (loop :for i :below (array-total-size *discs*)
+        :do (setf (cadr (elt *discs* i)) (mod (incf (cadr (elt *discs* i))) (car (elt *discs* i))))))
+
+(defun slots-aligned-p ()
+  (loop :with aligned = t
+        :for i :below (array-total-size *discs*)
+        :if (not (zerop (test-advance-disc i i)))
+          :do (setf aligned nil)
+        :finally (return aligned)))
+
+(defun find-aligned-slots ()
+  (loop :for i :from 0
+        :do (advance-discs)
+        :until (slots-aligned-p)
+        :finally (return i)))
+
+(defun part1 ()
+  (find-aligned-slots))
