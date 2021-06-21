03b978384113aa56540f8cfb46dc2d3a7545948e
RichJames <risajajr@gmail.com>
2021-06-21 17:21:56 -0500

    Solution to part 2, Day 20.

--- 2016/Day20.lisp
+++ 2016/Day20.lisp
@@ -25,7 +25,21 @@
 
       (find-ip 0 ip-ranges))))
 
+;;; ***** Part 2 *****
 
+(defun count-allowed-ips ()
+  (let* ((ip-ranges (sort (load-input "~/quicklisp/local-projects/rich/advent/2016/Day20.txt") #'< :key #'car))
+         (highest-allowed-ip 4294967295)
+         (highest-blocked (second (car (sort (copy-list ip-ranges) #'> :key #'cadr)))))
+
+    (labels ((find-ip (candidate ip-list acc)
+               (cond ((> candidate highest-allowed-ip) acc)
+                     ((null ip-list) (+ acc (- highest-allowed-ip highest-blocked)))
+                     ((> candidate (second (car ip-list))) (find-ip candidate (cdr ip-list) acc))
+                     ((<= (first (car ip-list)) candidate (second (car ip-list))) (find-ip (1+ (second (car ip-list))) (cdr ip-list) acc))
+                     (t (find-ip (1+ candidate) ip-list (1+ acc))))))
+
+      (find-ip 0 ip-ranges 0)))))
 
 
 
